975c67935671d780f64f3751baea88c7b6e273b4
Mike Minns <mminns@atlassian.com>
2017-07-12 20:41:49 +0100

    Tweaked the script to add a CI specific tag as well as a standard version one.

--- appveyor-tag-repository.ps1
+++ appveyor-tag-repository.ps1
@@ -5,5 +5,6 @@ git config --global credential.helper store
 Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GitUsername):$($env:GitPassword)@github.com`n"
 
 git remote add github https://$($env:GitUsername)@github.com/mminns/Mercurial-Credential-Manager-for-Windows.git
-git tag $($env:appveyor_build_version) $($env:APPVEYOR_REPO_COMMIT)
+git tag "v$($env:appveyor_build_version)" $($env:APPVEYOR_REPO_COMMIT)
+git tag "v$($env:appveyor_build_version).ci" $($env:APPVEYOR_REPO_COMMIT)
 git push github --tags --quiet

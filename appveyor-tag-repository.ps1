3e957e6ba6fcfce35e9c20d2d56b8932d9125000
Mike Minns <mminns@atlassian.com>
2017-07-12 20:48:23 +0100

    fix tagging

--- appveyor-tag-repository.ps1
+++ appveyor-tag-repository.ps1
@@ -6,5 +6,4 @@ Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GitUsername):$($
 
 git remote add github https://$($env:GitUsername)@github.com/mminns/Mercurial-Credential-Manager-for-Windows.git
 git tag "v$($env:appveyor_build_version)" $($env:APPVEYOR_REPO_COMMIT)
-git tag "v$($env:appveyor_build_version).ci" $($env:APPVEYOR_REPO_COMMIT)
 git push github --tags --quiet

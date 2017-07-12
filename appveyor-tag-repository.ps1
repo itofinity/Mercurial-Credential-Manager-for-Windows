1be38285401166cbefd72eb06a73b3b5fecc6ea8
Mike Minns <mminns@atlassian.com>
2017-07-12 20:05:44 +0100

    refacto script to target github

--- appveyor-tag-repository.ps1
+++ appveyor-tag-repository.ps1
@@ -2,8 +2,8 @@ git config --global user.email "$($env:GitEmail)"
 git config --global user.name "$($env:GitUsername)"
 git config --global credential.helper store
 
-Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GitUsername):$($env:GitPassword)@bitbucket.org`n"
+Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:GitUsername):$($env:GitPassword)@github.com`n"
 
-git remote add bitbucket https://$($env:GitUsername)@bitbucket.org/$($env:APPVEYOR_REPO_NAME).git
+git remote add github https://$($env:GitUsername)@github.com/mminns/Mercurial-Credential-Manager-for-Windows.git
 git tag $($env:appveyor_build_version) $($env:APPVEYOR_REPO_COMMIT)
-git push bitbucket --tags --quiet
+git push github --tags --quiet
